d1bd08ec745f50fc07ae64d75ff0817b41d87d94
cjbell <cjbell1988@gmail.com>
2021-03-11 01:05:45 -0500

    fix: fix deploy issues with exporting a static build

--- layouts/mdxLayout/index.tsx
+++ layouts/mdxLayout/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { MDXProvider } from "@mdx-js/react";
 import { DocsLayout } from "../DocsLayout";
-import NextImage from "next/image";
 import { FrontMatter } from "../../types";
 import { CodeBlock } from "../../components/CodeBlock";
 
 const components = {
   pre: CodeBlock,
-  NextImage,
 };
 
 const Layout: React.FC<{ frontMatter: FrontMatter }> = (props) => (
