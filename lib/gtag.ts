5452eb18fe79d680365fa00816a0c852a7a4d36f
cjbell <cjbell1988@gmail.com>
2021-06-23 16:35:13 -0600

    chore: fix tsc error

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,6 +2,7 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
+  // @ts-ignore
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   });
