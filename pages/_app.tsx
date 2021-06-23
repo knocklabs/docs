29ec4259bbedd7f0adcc370aebe589be8d245843
cjbell <cjbell1988@gmail.com>
2021-06-23 16:21:33 -0600

    feat(KNO-265): add google analytics to docs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,25 @@ import "@fontsource/inter";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
+import { useRouter } from "next/router";
 import "../styles/index.css";
+import * as gtag from "../lib/gtag";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
