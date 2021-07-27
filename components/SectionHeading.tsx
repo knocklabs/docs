b5129942f10105c283b7f93e19f7450a1499bb2a
Thomas <thomas.whyyou@gmail.com>
2021-07-27 19:31:31 -0400

    Switch to pure css based behavior

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -2,39 +2,33 @@ import React from "react";
 import { useRouter } from "next/router";
 import cn from "classnames";
 
+const CLASS_SELECTOR = "section-heading";
+
 type HeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
 type Props = React.HTMLProps<HTMLHeadingElement> & {
   tag: HeadingTag;
 };
 
-const SectionHeading: React.FC<Props> = ({ id, tag, children, ...rest }) => {
+const SectionHeading: React.FC<Props> = ({ id, tag, children, className, ...rest }) => {
   const Tag = `${tag}` as keyof Pick<JSX.IntrinsicElements, HeadingTag>;
-
-  const [isAnchorVisible, setIsAnchorVisible] = React.useState(false);
   const { pathname } = useRouter();
-  const href = id ? `${pathname}#${id}` : pathname;
+  const href = id ? `#${id}` : pathname;
 
   return (
     <Tag
       {...rest}
-      onMouseEnter={() => setIsAnchorVisible(true)}
-      onMouseLeave={() => setIsAnchorVisible(false)}
+      id={id}
+      className={cn(CLASS_SELECTOR, className)}
     >
       {children}
 
       <a
         id={id}
         href={href}
-        style={{ fontSize: "80%" }}
-        className={cn(
-          isAnchorVisible ? "opacity-30" : "opacity-0",
-          "hover:opacity-60",
-          "absolute -left-6 pr-2 cursor-pointer transition-opacity"
-        )}
-      >
-        #
-      </a>
+        style={{ color: "inherit", textDecoration: "none" }}
+        className="absolute -left-6 pr-3 cursor-pointer"
+      />
     </Tag>
   );
 };
