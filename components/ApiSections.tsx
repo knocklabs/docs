a88e5ae9c90235939e0f76a4650bf000c80e70a9
Chris Bell <cjbell1988@gmail.com>
2022-02-22 13:44:13 -0600

    some style updates and fixes

--- components/ApiSections.tsx
+++ components/ApiSections.tsx
@@ -9,15 +9,6 @@ export const Section = ({ title, slug, children }) => (
   </section>
 );
 
-export const SectionFlexCol = ({ title, slug, children }) => (
-  <section className="api-docs-section border-b border-gray-200 py-16">
-    <SectionHeading tag="h2" id={slug || title.toLowerCase()} className="mb-6">
-      {title}
-    </SectionHeading>
-    <div className="flex flex-col">{children}</div>
-  </section>
-);
-
 export const ContentColumn = ({ children }) => (
   <div className="api-docs-section__col md:pr-5 flex-grow-0 flex-shrink-0 w-full md:w-1/2">
     <div className="prose-sm lg:prose">{children}</div>
@@ -31,10 +22,10 @@ export const ExampleColumn = ({ children }) => (
 );
 
 export const ErrorExample = ({ title, description }) => (
-  <div className="flex-col pb-8">
-    <span className="bg-code-background text-code rounded text-sm font-normal py-0.75 px-1.5 font-mono inline-block border-1 border-gray-200">
+  <div className="flex-col pt-6 mt-6 border-gray-200 border-t">
+    <span className="bg-code-background text-code rounded text-sm font-normal py-0.75 px-1.5 font-mono inline-block">
       {title}
     </span>
-    <p className="pt-1">{description}</p>
+    <span className="block pt-0 mt-1 text-gray-700 text-sm">{description}</span>
   </div>
 );
