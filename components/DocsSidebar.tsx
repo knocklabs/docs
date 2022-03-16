3386f766a2105551b1440ade3c0177186ae88ec8
Chris Bell <cjbell1988@gmail.com>
2022-03-16 12:28:46 -0400

    chore: add slack community and back to dashboard links (#67)

--- components/DocsSidebar.tsx
+++ components/DocsSidebar.tsx
@@ -1,17 +1,31 @@
 import { Icon } from "@chakra-ui/react";
 import Link from "next/link";
-import { IoTerminal } from "react-icons/io5";
+import { IoLogoSlack, IoTerminal } from "react-icons/io5";
 import sidebarContent from "../data/sidebar";
 import Sidebar from "./Sidebar";
 
 const DocsSidebar = () => (
   <Sidebar content={sidebarContent}>
-    <Link href="/reference" passHref>
-      <a className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-900">
-        <Icon as={IoTerminal} mr={2} />
-        API reference &#8594;
-      </a>
-    </Link>
+    <div className="space-y-2">
+      <Link href="/reference" passHref>
+        <a className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-900">
+          <Icon as={IoTerminal} mr={2} />
+          API reference
+          <span className="ml-auto">&#8594;</span>
+        </a>
+      </Link>
+
+      <Link
+        href="https://join.slack.com/t/knockcustomers/shared_invite/zt-13zos4lkx-FWnqvhJjhlbwlWmKelIj5Q"
+        passHref
+      >
+        <a className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-900">
+          <Icon as={IoLogoSlack} mr={2} />
+          Join our community
+          <span className="ml-auto">&#8594;</span>
+        </a>
+      </Link>
+    </div>
   </Sidebar>
 );
 
