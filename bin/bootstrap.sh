9be55521f36ca80da507b0c37fbe3c0ca1598a07
Billy Ceskavich <bceskavich@gmail.com>
2022-05-20 13:42:17 -0400

    chore(kno-1375): bootstrap script update (#88)
    - Ensure asdf executable is added to shell run commands file upon fresh
      install via Homebrew. For uncommon shells, prompt the user to do this
      manually via the asdf docs.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,8 +4,23 @@ set -o errexit
 if ! command -v asdf &> /dev/null
 then
   echo "Installing asdf"
-
   brew install asdf
+
+  # After a fresh asdf install, we need to add the asdf executable to our shell's
+  # run commands file. This will ensure asdf-installed executables are added to
+  # our $PATH. We handle this step for bash and zsh.
+  if [ "$SHELL" = "/bin/zsh" ]; then
+    echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ~/.zshrc
+  elif [ "$SHELL" = "/bin/bash" ]; then
+    echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ~/.bash_profile
+  else
+    echo ""
+    echo "Unknown shell!"
+    echo "You must manually add the asdf executable to your shell's run-commands file before proceeding."
+    echo "See the asdf docs for more info: http://asdf-vm.com/guide/getting-started.html#_3-install-asdf"
+
+    exit 1
+  fi
 fi
 
 asdf plugin add nodejs || true
