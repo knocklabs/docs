09e0f26129c6d55eb4fc3b59c23688f897f0a6e1
Chris Bell <cjbell1988@gmail.com>
2022-08-09 14:53:47 -0500

    fix tsc issues

--- components/Autocomplete.tsx
+++ components/Autocomplete.tsx
@@ -58,9 +58,10 @@ interface ResultItem extends BaseItem {
 }
 
 const Autocomplete = () => {
-  const algoliaAppId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
-  const algoliaSearchApiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY;
-  const algoliaIndex = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME;
+  const algoliaAppId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || "";
+  const algoliaSearchApiKey =
+    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || "";
+  const algoliaIndex = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME || "";
   const [autocompleteState, setAutocompleteState] =
     useState<AutocompleteState<BaseItem> | null>(null);
 
