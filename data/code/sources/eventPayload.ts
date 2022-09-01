e305187a71bd7837f8724210eb81f885f4b1225f
Meryl Dakin <meryl.k.dakin@gmail.com>
2022-09-01 17:56:45 -0400

    feat(kno-1986): add event trigger docs (#140)

---
+++ data/code/sources/eventPayload.ts
@@ -0,0 +1,10 @@
+const eventPayload = `
+{
+    "properties": {
+        "commenter": {
+            "id": 123,
+            "name": "John Hammond"
+        }
+    }
+}`;
+export default eventPayload;
