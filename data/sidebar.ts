b71612185f08dd41e9251bfa4e35e7b45bde2824
Sam Seely <samseely@gmail.com>
2022-12-06 14:49:57 -0500

    updates to auto join docs

--- data/sidebar.ts
+++ data/sidebar.ts
@@ -127,7 +127,6 @@ const sidebarContent: SidebarSection[] = [
       { slug: "/managing-members", title: "Managing members" },
       { slug: "/roles-and-permissions", title: "Roles and permissions" },
       { slug: "/audit-logs", title: "Audit logs" },
-      { slug: "/auto-join", title: "Auto-join" },
     ],
   },
 ];
