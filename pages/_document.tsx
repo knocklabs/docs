42b81a244d327cff3865a14620485d41fa38669f
Billy Ceskavich <bceskavich@gmail.com>
2022-12-21 16:10:14 -0500

    feat(kno-2552): add link and open tracking docs (#187)
    * feat(kno-2552): add link and open tracking docs
    
    - Adds new guide on link + open tracking and adds links to it throughout
    - Fills in missing text/links in the concepts overview page
    - Adds a Messages concept overview page
    - Fixes the integrations sidebar so that the overview page has a navigation link
    
    * chore: add screenshots
    
    * update styling on images
    
    * Update pages/send-and-manage-data/messages.mdx
    
    Co-authored-by: Sam Seely <sam@knock.app>
    
    * Update pages/send-notifications/tracking.mdx
    
    Co-authored-by: Sam Seely <sam@knock.app>
    
    * Update pages/send-notifications/tracking.mdx
    
    Co-authored-by: Sam Seely <sam@knock.app>
    
    * Update pages/send-notifications/tracking.mdx
    
    Co-authored-by: Sam Seely <sam@knock.app>
    
    * remove billing ntoe
    
    * Add loom walkthru embed
    
    * Update webhook event payload example
    
    Co-authored-by: Sam Seely <samseely@gmail.com>
    Co-authored-by: Sam Seely <sam@knock.app>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,6 +24,7 @@ export default class DocsDocument extends Document {
             }}
           />
           <script
+            // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
               __html: `
           (function(d, u, h, s) {
