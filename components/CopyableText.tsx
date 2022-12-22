0cb6015833569443906f142504b4206e618990c6
Brent Anderson <brent@knock.app>
2022-12-22 14:37:51 -0600

    feat(kno-2580): Datadog dashboard instructions (#191)

---
+++ components/CopyableText.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { IoCheckmark, IoCopy } from "react-icons/io5";
+import useClipboard from "react-use-clipboard";
+
+const CopyableText = ({ content, label }) => {
+  const [isCopied, setCopied] = useClipboard(content, {
+    successDuration: 2000,
+  });
+
+  return (
+    <span>
+      <button
+        type="button"
+        onClick={setCopied}
+        className="flex inline h-full items-center font-medium text-[14px] leading-[21px] px-3 py-1 border text-brand hover:text-brand-dark border-brand rounded-md font-semibold"
+      >
+        {label}&nbsp;{isCopied ? <IoCheckmark /> : <IoCopy />}
+      </button>
+    </span>
+  );
+};
+
+export default CopyableText;
