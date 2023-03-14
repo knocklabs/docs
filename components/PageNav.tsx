b4f0cf4913fe79913922bf8b3adc00f479aaf625
Chris Bell <cjbell1988@gmail.com>
2023-03-14 13:25:16 -0400

    chore: user docs updates (#225)

--- components/PageNav.tsx
+++ components/PageNav.tsx
@@ -104,7 +104,7 @@ const PageNav: React.FC<Props> = ({ title }) => {
   return (
     <aside
       className="fixed top-30 pl-5 w-64 overflow-y-auto"
-      style={{ height: "calc(100vh - 225px)" }}
+      style={{ height: "calc(100vh - 95px)" }}
     >
       <h5 className="text-xs uppercase text-gray-900 dark:text-gray-500 font-semibold tracking-wider mb-3">
         On this page
