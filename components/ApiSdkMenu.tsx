8568b9af47d7743b892a22083f6f3e6b95fb61a7
Chris Bell <cjbell1988@gmail.com>
2023-04-20 10:54:02 -0400

    feat: add correct links

--- components/ApiSdkMenu.tsx
+++ components/ApiSdkMenu.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as Popover from "@radix-ui/react-popover";
-import FocusLock from "react-focus-lock";
-import { IoChevronDown, IoOpen, IoOpenOutline } from "react-icons/io5";
+import { IoChevronDown, IoOpenOutline } from "react-icons/io5";
 import Link from "next/dist/client/link";
 import { useRouter } from "next/router";
 
@@ -13,14 +12,14 @@ const PageLinks = {
 };
 
 const SdkLinks = {
-  NodeJS: "https://",
-  Python: "",
-  Ruby: "",
-  Go: "",
-  PHP: "",
-  Java: "",
-  "C# (dotnet)": "",
-  Elixir: "",
+  "Node JS": "https://github.com/knocklabs/knock-node",
+  Python: "https://github.com/knocklabs/knock-python",
+  Ruby: "https://github.com/knocklabs/knock-ruby",
+  Go: "https://github.com/knocklabs/knock-go",
+  PHP: "https://github.com/knocklabs/knock-php",
+  Java: "https://github.com/knocklabs/knock-java",
+  "C# (dotnet)": "https://github.com/knocklabs/knock-dotnet",
+  Elixir: "https://github.com/knocklabs/knock-elixir",
 };
 
 const ApiSdkMenu = () => {
