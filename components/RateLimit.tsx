3f4b0cd8ffaff11f64f745f37159e1f5d784b024
Billy Ceskavich <bceskavich@gmail.com>
2023-07-27 10:36:51 -0400

    update with latest scoping

--- components/RateLimit.tsx
+++ components/RateLimit.tsx
@@ -3,10 +3,9 @@ import cn from "classnames";
 type Props = {
   tier: 1 | 2 | 3 | 4 | 5;
   isBatch?: boolean;
-  on?: string;
 };
 
-const RateLimit: React.FC<Props> = ({ tier, on, isBatch = false }) => (
+const RateLimit: React.FC<Props> = ({ tier, isBatch = false }) => (
   <span
     className={cn({
       "text-xs font-medium text-gray-600 dark:text-gray-300 border border-transparent font-mono rounded p-1 center mr-3":
@@ -20,7 +19,6 @@ const RateLimit: React.FC<Props> = ({ tier, on, isBatch = false }) => (
   >
     {isBatch ? "Batch Tier " : "Tier "}
     {tier}
-    {on ? ` → ${on}` : ""}
   </span>
 );
 
