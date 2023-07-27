bf41f459acfa6839506be81386405a594e8c5797
Billy Ceskavich <bceskavich@gmail.com>
2023-07-27 10:50:57 -0400

    feat(kno-4011): batch deduplication rate limit docs (#293)

--- components/RateLimit.tsx
+++ components/RateLimit.tsx
@@ -2,9 +2,10 @@ import cn from "classnames";
 
 type Props = {
   tier: 1 | 2 | 3 | 4 | 5;
+  isBatch?: boolean;
 };
 
-const RateLimit: React.FC<Props> = ({ tier }) => (
+const RateLimit: React.FC<Props> = ({ tier, isBatch = false }) => (
   <span
     className={cn({
       "text-xs font-medium text-gray-600 dark:text-gray-300 border border-transparent font-mono rounded p-1 center mr-3":
@@ -16,7 +17,8 @@ const RateLimit: React.FC<Props> = ({ tier }) => (
       "bg-red-100 dark:bg-transparent dark:border-red-500": tier === 1,
     })}
   >
-    Tier {tier}
+    {isBatch ? "Batch Tier " : "Tier "}
+    {tier}
   </span>
 );
 
