894b7e236e8f8582051a46cff06cc18f813a1ea0
Franco Borrelli <franco@mimiquate.com>
2023-10-24 18:33:13 -0300

    feat(KNO-4267): update support modal

--- components/SupportModal.tsx
+++ components/SupportModal.tsx
@@ -97,88 +97,91 @@ const SupportModal: React.FC<Props> = ({ currentUser, currentAccount }) => {
         <Dialog.Portal>
           <Dialog.Overlay className="fixed inset-0 bg-black/50 " />
           <Dialog.Title />
-          <Dialog.Content className="py-4 px-6 text-gray-900 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm">
-            <Dialog.Title>
-              <div className="flex flex-column items-center justify-between">
-                <h2 className="text-gray-600 font-semibold text-[16px] dark:text-white">
-                  Contact support
-                </h2>
-                <button
-                  type="button"
-                  className="text-gray-600 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600 rounded-md"
-                  onClick={() => setIsOpen(false)}
-                >
-                  <IoCloseOutline style={{ fontSize: "24px" }} />
-                </button>
-              </div>
-            </Dialog.Title>
-
-            <Dialog.Description>
-              <p className="font-medium text-[14px] text-gray-600 dark:text-white">
-                What issue are you seeing? If you're reporting a bug, what are
-                the steps you took so we can reproduce the behavior?
-              </p>
-            </Dialog.Description>
-
-            <div className="mt-5">
-              <form onSubmit={() => null}>
-                <div className="h-[200px]">
-                  <textarea
-                    className="text-gray-600 font-normal p-2 text-[14px] dark:text-white w-full h-full dark:border-gray-700 border border-gray-200 dark:bg-gray-800 focus-visible:outline-none rounded-sm"
-                    value={feedbackBody}
-                    onChange={(e) => setFeedbackBody(e.target.value)}
-                    onKeyDown={(e) => isSubmitHotkey(e) && handleSubmit()}
-                    ref={textAreaRef}
-                    placeholder="Help us improve this page."
-                    style={{ resize: "vertical" }}
-                  />
+          <Dialog.Content className="text-gray-900 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm">
+            <div className="py-4 px-6">
+              <Dialog.Title>
+                <div className="flex flex-column items-center justify-between">
+                  <h2 className="text-gray-600 font-semibold text-[16px] dark:text-white">
+                    Contact support
+                  </h2>
+                  <button
+                    type="button"
+                    className="text-gray-600 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600 rounded-md"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    <IoCloseOutline style={{ fontSize: "24px" }} />
+                  </button>
                 </div>
-                <div className="flex mt-5 items-center justify-between">
-                  <div className="flex items-center mr-5">
-                    <input
-                      className="text-gray-600 bg-white border border-gray-200 p-1 h-7 w-[215px] text-[12px] rounded-sm dark:bg-gray-800 dark:border-gray-700 dark:text-white focus-visible:outline-none"
-                      placeholder="youremail@example.com"
-                      onChange={(e) => setUserEmail(e.target.value)}
+              </Dialog.Title>
+
+              <Dialog.Description>
+                <p className="font-medium text-[14px] text-gray-600 dark:text-white">
+                  What issue are you seeing? If you're reporting a bug, what are
+                  the steps you took so we can reproduce the behavior?
+                </p>
+              </Dialog.Description>
+            </div>
+            <div className="py-4 px-6 bg-gray-50 border-t dark:border-t-black dark:bg-gray-900">
+              <div className="mt-5">
+                <form onSubmit={() => null}>
+                  <div className="h-[200px]">
+                    <textarea
+                      className="text-gray-600 font-normal p-2 text-[14px] dark:text-white w-full h-full dark:border-gray-700 border border-gray-200 dark:bg-gray-800 focus-visible:outline-none rounded-sm"
+                      value={feedbackBody}
+                      onChange={(e) => setFeedbackBody(e.target.value)}
+                      onKeyDown={(e) => isSubmitHotkey(e) && handleSubmit()}
+                      ref={textAreaRef}
+                      placeholder="Help us improve this page."
+                      style={{ resize: "vertical" }}
                     />
-                    <RadioGroup.Root
-                      name="feedback-category"
-                      className="flex space-x-1 ml-2"
-                    >
-                      {Array.from(FEEDBACK_CATEGORIES.keys()).map((emoji) => (
-                        <RadioGroup.Item
-                          key={emoji}
-                          value={emoji}
-                          className="text-center w-7 h-7 border border-gray-200 dark:border-gray-600 rounded-md focus:outline-blue-400 radix-state-checked:!border-brand"
-                        >
-                          {emoji}
-                        </RadioGroup.Item>
-                      ))}
-                    </RadioGroup.Root>
                   </div>
-                  <div className="flex items-center">
-                    <p className="text-gray-600 font-normal text-[12px] dark:text-white">
-                      {" "}
-                      You can also email us at{" "}
-                      <a
-                        href="mailto:support@knock.app"
-                        target="_blank"
-                        onClick={() => setIsOpen(false)}
-                        className="text-[12px] text-gray-700 dark:text-white hover:underline"
-                        rel="noreferrer"
+                  <div className="flex mt-5 items-center justify-between">
+                    <div className="flex items-center mr-5">
+                      <input
+                        className="text-gray-600 bg-white border border-gray-200 p-1 h-7 w-[215px] text-[12px] rounded-sm dark:bg-gray-800 dark:border-gray-700 dark:text-white focus-visible:outline-none"
+                        placeholder="youremail@example.com"
+                        onChange={(e) => setUserEmail(e.target.value)}
+                      />
+                      <RadioGroup.Root
+                        name="feedback-category"
+                        className="flex space-x-1 ml-2"
                       >
-                        support@knock.app.
-                      </a>
-                    </p>
-                    <button
-                      type="submit"
-                      disabled={!hasFeedback || !hasUserEmail || isLoading}
-                      className="bg-brand text-white text-[14px] px-2 py-1 ml-3 rounded-md disabled:opacity-40 disabled:cursor-not-allowed"
-                    >
-                      {isLoading ? "Sending" : "Send"}
-                    </button>
+                        {Array.from(FEEDBACK_CATEGORIES.keys()).map((emoji) => (
+                          <RadioGroup.Item
+                            key={emoji}
+                            value={emoji}
+                            className="text-center w-7 h-7 border border-gray-200 dark:border-gray-600 rounded-md focus:outline-blue-400 radix-state-checked:!border-brand"
+                          >
+                            {emoji}
+                          </RadioGroup.Item>
+                        ))}
+                      </RadioGroup.Root>
+                    </div>
+                    <div className="flex items-center">
+                      <p className="text-gray-600 font-normal text-[12px] dark:text-white">
+                        {" "}
+                        You can also email us at{" "}
+                        <a
+                          href="mailto:support@knock.app"
+                          target="_blank"
+                          onClick={() => setIsOpen(false)}
+                          className="text-[12px] text-gray-700 dark:text-white hover:underline"
+                          rel="noreferrer"
+                        >
+                          support@knock.app.
+                        </a>
+                      </p>
+                      <button
+                        type="submit"
+                        disabled={!hasFeedback || !hasUserEmail || isLoading}
+                        className="bg-brand text-white text-[14px] px-2 py-1 ml-3 rounded-md disabled:opacity-40 disabled:cursor-not-allowed"
+                      >
+                        {isLoading ? "Sending" : "Send"}
+                      </button>
+                    </div>
                   </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </Dialog.Content>
         </Dialog.Portal>
