333e447ee536879a6bd7c60c1b71bc01d7c77472
Matt Kufchak <matt.kufchak@gmail.com>
2024-02-07 17:26:44 -0600

    add install commands for currently-linked SDKs

--- components/MultiLangCodeBlock.tsx
+++ components/MultiLangCodeBlock.tsx
@@ -19,6 +19,7 @@ const EVENT_NAME = "language.change";
 
 const snippets = {
   "api.idempotency": require("../data/code/api/idempotency").default,
+  "install": require("../data/code/install").default,
   // Users
   "users.bulkIdentify": require("../data/code/users/bulk-identify").default,
   "users.bulkDelete": require("../data/code/users/bulk-delete").default,
---
+++ data/code/install.ts
@@ -0,0 +1,54 @@
+const languages = {
+    javascript: `
+    npm install @knocklabs/node
+  `,
+    python: `
+    pip install knockapi
+  `,
+    ruby: `
+    gem install knockapi
+  `,
+    go: `
+    go get github.com/knocklabs/knock-go/knock
+  `,
+    php: `
+    composer require knocklabs/knock-php php-http/guzzle7-adapter
+  `,
+    java: `
+  // To build.gradle:
+  dependencies {
+    implementation 'app.knock.api:knock-client:0.2.10'
+  }
+
+// To maven.xml:
+  <dependencies>
+    <!-- more dependencies here -->
+    <dependency>
+        <groupId>app.knock.api</groupId>
+        <artifactId>knock-client</artifactId>
+        <version>0.2.10</version>
+    </dependency>
+    <!-- more dependencies here -->
+  </dependencies>
+  `,
+    csharp: `
+  // Via the NuGet Package Manager
+  nuget install Knock.net
+
+// Via the .NET Core Command Line Tools
+  dotnet add package Knock.net
+
+// Via Visual Studio IDE
+  Install-Package Knock.net
+  `,
+    elixir: `
+# Add to mix.exs
+def deps do
+  [
+    {:knock, "~> 0.4"}
+  ]
+end
+  `,
+  };
+  
+  export default languages;
