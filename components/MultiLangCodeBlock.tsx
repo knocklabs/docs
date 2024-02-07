2d640f576cfdb53446d7715cc291609e3cfd2dc9
Matt Kufchak <matt.kufchak@gmail.com>
2024-02-07 17:47:23 -0600

    rename code block and move file to satisfy prettier

--- components/MultiLangCodeBlock.tsx
+++ components/MultiLangCodeBlock.tsx
@@ -19,7 +19,7 @@ const EVENT_NAME = "language.change";
 
 const snippets = {
   "api.idempotency": require("../data/code/api/idempotency").default,
-  "install": require("../data/code/install").default,
+  "sdks.install": require("../data/code/sdks/install").default,
   // Users
   "users.bulkIdentify": require("../data/code/users/bulk-identify").default,
   "users.bulkDelete": require("../data/code/users/bulk-delete").default,
