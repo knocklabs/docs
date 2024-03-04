30689c480bcaf50cde18bc2dcd72c5eff94815f1
Chris Bell <cjbell1988@gmail.com>
2024-03-04 15:46:05 -0500

    chore: add dynamic sitemap and robots (#418)

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+import { BASE_URL } from "../lib/constants";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${BASE_URL}/sitemap.xml`,
+  };
+}
