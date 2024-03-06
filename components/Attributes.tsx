fd3cbc23176cf158cc01df492ea351aaedaf24a1
Matt Kufchak <matt@knock.app>
2024-03-05 22:41:47 -0600

    format

--- components/Attributes.tsx
+++ components/Attributes.tsx
@@ -10,16 +10,16 @@ const Attribute = ({ name, type, description, typeSlug }) => (
       <span className="font-mono text-xs">{name}</span>
       <span className="font-semibold text-gray-500 dark:text-gray-300 text-xs ml-2 py-0.5 px-1 border border-gray-100 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
         {/* Pass an optional `typeSlug` to link the `type` to its definition in the docs*/}
-        {!!typeSlug ? 
-          <a 
-            href={typeSlug} 
+        {!!typeSlug ? (
+          <a
+            href={typeSlug}
             className="not-prose transition-colors hover:text-gray-900 dark:hover:text-gray-100 text-brand hover:!text-brand-dark"
-            >
+          >
             {type}
           </a>
-        : 
+        ) : (
           type
-        }
+        )}
       </span>
     </span>
     <span className="block text-sm mt-0 text-gray-600 dark:text-gray-300">
