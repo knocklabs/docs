497a126a70f0553f0b4c59050b7e89369acca96b
Jeff Everhart <Jeffeverhart383@gmail.com>
2024-05-10 12:09:13 -0400

    update all multilang node exs to use import (#495)

--- data/code/messages/list.ts
+++ data/code/messages/list.ts
@@ -1,6 +1,6 @@
 const languages = {
   javascript: `
-const { Knock } = require("@knocklabs/node");
+import { Knock } from "@knocklabs/node";
 const knockClient = new Knock("sk_12345");
 
 const messages = await knockClient.messages.list()
