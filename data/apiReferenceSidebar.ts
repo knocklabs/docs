42b0e31f673871680b46fdb8949db179a401888e
Matt Kufchak <matt@knock.app>
2024-07-29 09:29:13 -0500

    chore: re-order api ref sidebar and content to align. add missing endpoint. (#578)
    * chore: re-order api ref sidebar and content to align. add missing endpoint
    
    * chore: format

--- data/apiReferenceSidebar.ts
+++ data/apiReferenceSidebar.ts
@@ -18,21 +18,6 @@ const sidebarContent: SidebarSection[] = [
       { slug: "#error-codes", title: "Common error codes" },
     ],
   },
-  {
-    title: "Users",
-    slug: "/reference",
-    pages: [
-      { slug: "#users", title: "Overview" },
-      { slug: "#identify-user", title: "Identify a user" },
-      { slug: "#list-users", title: "List users" },
-      { slug: "#get-user", title: "Get a user" },
-      { slug: "#get-user-messages", title: "Get messages" },
-      { slug: "#delete-user", title: "Delete a user" },
-      { slug: "#merge-user", title: "Merge users" },
-      { slug: "#bulk-identify-users", title: "Bulk identify users" },
-      { slug: "#bulk-delete-users", title: "Bulk delete users" },
-    ],
-  },
 
   {
     title: "Workflows",
@@ -49,11 +34,77 @@ const sidebarContent: SidebarSection[] = [
   },
 
   {
+    title: "Messages",
+    slug: "/reference",
+    pages: [
+      { slug: "#messages", title: "Overview" },
+      { slug: "#list-messages", title: "List messages" },
+      { slug: "#get-a-message", title: "Get a message" },
+      { slug: "#get-message-events", title: "Get events" },
+      { slug: "#get-message-activities", title: "Get activities" },
+      { slug: "#get-message-content", title: "Get content" },
+      { slug: "#batch-get-message-content", title: "Batch get content" },
+      { slug: "#get-message-delivery-logs", title: "Get delivery logs" },
+      { slug: "#update-message-status", title: "Update status" },
+      { slug: "#undo-message-status", title: "Remove status" },
+      { slug: "#batch-update-message-status", title: "Batch change status" },
+      {
+        slug: "#bulk-update-channel-message-status",
+        title: "Bulk change status under channel",
+      },
+    ],
+  },
+
+  {
     title: "Feeds",
     slug: "/reference",
     pages: [
       { slug: "#feeds", title: "Overview" },
       { slug: "#get-feed", title: "Get a feed for a user" },
+    ],
+  },
+
+  {
+    title: "Users",
+    slug: "/reference",
+    pages: [
+      { slug: "#users", title: "Overview" },
+      { slug: "#identify-user", title: "Identify a user" },
+      { slug: "#list-users", title: "List users" },
+      { slug: "#get-user", title: "Get a user" },
+      { slug: "#get-user-messages", title: "Get user messages" },
+      { slug: "#delete-user", title: "Delete a user" },
+      { slug: "#merge-user", title: "Merge users" },
+      { slug: "#bulk-identify-users", title: "Bulk identify users" },
+      { slug: "#bulk-delete-users", title: "Bulk delete users" },
+    ],
+  },
+
+  {
+    title: "Objects",
+    slug: "/reference",
+    pages: [
+      { slug: "#objects", title: "Overview" },
+      { slug: "#list-objects", title: "List objects" },
+      { slug: "#get-object", title: "Get an object" },
+      { slug: "#get-object-messages", title: "Get object messages" },
+      { slug: "#set-object", title: "Set an object" },
+      { slug: "#delete-object", title: "Delete an object" },
+      { slug: "#bulk-set-objects", title: "Bulk set objects" },
+      { slug: "#bulk-delete-objects", title: "Bulk delete objects" },
+    ],
+  },
+
+  {
+    title: "Subscriptions",
+    slug: "/reference",
+    pages: [
+      { slug: "#subscriptions", title: "Overview" },
+      { slug: "#list-subscriptions", title: "List subscriptions" },
+      { slug: "#get-user-subscriptions", title: "Get user subscriptions" },
+      { slug: "#add-subscriptions", title: "Add subscriptions" },
+      { slug: "#bulk-add-subscriptions", title: "Bulk add subscriptions" },
+      { slug: "#delete-subscriptions", title: "Delete subscriptions" },
     ],
   },
 
@@ -71,38 +122,6 @@ const sidebarContent: SidebarSection[] = [
   },
 
   {
-    title: "Channel data",
-    slug: "/reference",
-    pages: [
-      { slug: "#channel-data", title: "Overview" },
-      { slug: "#get-user-channel-data", title: "Get user channel data" },
-      { slug: "#set-user-channel-data", title: "Set user channel data" },
-      { slug: "#unset-user-channel-data", title: "Remove user channel data" },
-      { slug: "#get-object-channel-data", title: "Get object channel data" },
-      { slug: "#set-object-channel-data", title: "Set object channel data" },
-      {
-        slug: "#unset-object-channel-data",
-        title: "Remove object channel data",
-      },
-    ],
-  },
-
-  {
-    title: "Objects",
-    slug: "/reference",
-    pages: [
-      { slug: "#objects", title: "Overview" },
-      { slug: "#list-objects", title: "List objects" },
-      { slug: "#get-object", title: "Get an object" },
-      { slug: "#get-object-messages", title: "Get messages" },
-      { slug: "#set-object", title: "Set an object" },
-      { slug: "#delete-object", title: "Delete an object" },
-      { slug: "#bulk-set-objects", title: "Bulk set objects" },
-      { slug: "#bulk-delete-objects", title: "Bulk delete objects" },
-    ],
-  },
-
-  {
     title: "Tenants",
     slug: "/reference",
     pages: [
@@ -111,19 +130,6 @@ const sidebarContent: SidebarSection[] = [
       { slug: "#get-tenant", title: "Get a tenant" },
       { slug: "#set-tenant", title: "Set a tenant" },
       { slug: "#delete-tenant", title: "Delete a tenant" },
-    ],
-  },
-
-  {
-    title: "Subscriptions",
-    slug: "/reference",
-    pages: [
-      { slug: "#subscriptions", title: "Overview" },
-      { slug: "#list-subscriptions", title: "List subscriptions" },
-      { slug: "#get-user-subscriptions", title: "Get user subscriptions" },
-      { slug: "#add-subscriptions", title: "Add subscriptions" },
-      { slug: "#bulk-add-subscriptions", title: "Bulk add subscriptions" },
-      { slug: "#delete-subscriptions", title: "Delete subscriptions" },
     ],
   },
 
@@ -142,22 +148,18 @@ const sidebarContent: SidebarSection[] = [
   },
 
   {
-    title: "Messages",
+    title: "Channel Data",
     slug: "/reference",
     pages: [
-      { slug: "#messages", title: "Overview" },
-      { slug: "#list-messages", title: "List messages" },
-      { slug: "#get-a-message", title: "Get a message" },
-      { slug: "#get-message-events", title: "Get events" },
-      { slug: "#get-message-activities", title: "Get activities" },
-      { slug: "#get-message-content", title: "Get content" },
-      { slug: "#batch-get-message-content", title: "Batch get content" },
-      { slug: "#get-message-delivery-logs", title: "Get delivery logs" },
-      { slug: "#update-message-status", title: "Update status" },
-      { slug: "#batch-update-message-status", title: "Batch change status" },
+      { slug: "#channel-data", title: "Overview" },
+      { slug: "#get-user-channel-data", title: "Get user channel data" },
+      { slug: "#set-user-channel-data", title: "Set user channel data" },
+      { slug: "#unset-user-channel-data", title: "Remove user channel data" },
+      { slug: "#get-object-channel-data", title: "Get object channel data" },
+      { slug: "#set-object-channel-data", title: "Set object channel data" },
       {
-        slug: "#bulk-update-channel-message-status",
-        title: "Bulk change status under channel",
+        slug: "#unset-object-channel-data",
+        title: "Remove object channel data",
       },
     ],
   },
