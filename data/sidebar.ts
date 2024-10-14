2d6036399e07d1a48afabc356733f964eb0c4189
Matthew Mikolay <commits@mattmik.com>
2024-10-14 16:55:08 -0400

    Move existing push notification doc

--- data/sidebar.ts
+++ data/sidebar.ts
@@ -289,7 +289,7 @@ const sidebarContent: SidebarSection[] = [
         pages: [
           { slug: "/overview", title: "Overview" },
           { slug: "/quick-start", title: "Quick start" },
-          { slug: "/push-notifications", title: "Push notifications" },
+          // TODO { slug: "/push-notifications", title: "Push notifications" }
           { slug: "/reference", title: "API reference" },
         ],
       },
@@ -299,7 +299,7 @@ const sidebarContent: SidebarSection[] = [
         pages: [
           // { slug: "/overview", title: "Overview" },
           // { slug: "/quick-start", title: "Quick start" },
-          // { slug: "/push-notifications", title: "Push notifications" },
+          { slug: "/push-notifications", title: "Push notifications" },
           { slug: "/reference", title: "API reference" },
         ],
       },
