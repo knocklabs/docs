8b4cc8344e125071ff81e893241b7b010bdd16d6
Matt Kufchak <matt@knock.app>
2025-02-13 18:08:11 -0600

    chore: first pass with SES page

--- components/Callout.tsx
+++ components/Callout.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const Callout = ({ emoji, children, text }) => (
   <div className="flex w-full rounded-md bg-gray-100 dark:bg-gray-800 p-4 pr-3 mb-6">
-    <div>
-      <div className="flex items-center justify-center h-6 w-6 rounded-sm shrink-0">
-        <div>
-          <div className="w-full h-full">{emoji}</div>
+    {emoji && (
+      <div>
+        <div className="flex items-center justify-center h-6 w-6 rounded-sm shrink-0">
+          <div>
+            <div className="w-full h-full">{emoji}</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="flex-col min-w-0 ml-2 w-full text-[14px] not-prose">
+    )}
+    <div className={`flex-col min-w-0 w-full text-[14px] not-prose ${emoji ? "ml-3" : "ml-1.5"}`}>
       {text || children}
     </div>
   </div>
