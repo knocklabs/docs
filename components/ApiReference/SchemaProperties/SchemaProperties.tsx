37ac9f6e77e96c925f0da3833fc2906f523b1b3f
Chris Bell <cjbell1988@gmail.com>
2025-04-09 18:13:48 -0400

    chore: fix union type display

--- components/ApiReference/SchemaProperties/SchemaProperties.tsx
+++ components/ApiReference/SchemaProperties/SchemaProperties.tsx
@@ -15,12 +15,12 @@ const SchemaProperties = ({ schema, hideRequired = false }: Props) => {
   return (
     <PropertyRow.Wrapper>
       {Object.entries(schema.properties || {}).map(
-        ([propertyName, property]) => (
+        ([propertyName, property]: [string, OpenAPIV3.SchemaObject]) => (
           <SchemaProperty
             key={propertyName}
             name={propertyName}
             schema={{
-              ...(property as OpenAPIV3.SchemaObject),
+              ...property,
               required: !hideRequired
                 ? property.required || schema.required?.includes(propertyName)
                 : false,
