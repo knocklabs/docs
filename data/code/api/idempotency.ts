3a6cd474040654233e4b92881a596bfed4088565
Matthew Mikolay <mmikolay@knock.app>
2025-09-26 17:50:33 -0400

    chore(KNO-9901): update idempotency code example for Go SDK

--- data/code/api/idempotency.ts
+++ data/code/api/idempotency.ts
@@ -140,7 +140,7 @@ result, _ := knockClient.Workflows.Trigger(ctx, "new-comment", knock.WorkflowTri
 	Actor:           param.New("3"),
 	CancellationKey: param.New("cancel_123"),
 	Tenant:          param.New("jurassic_world_employees"),
-}, option.WithHeader("Idempotency-Key", "123"))
+}, option.WithIdempotencyKey("123"))
 `,
   java: `
 import app.knock.api.client.KnockClient;
