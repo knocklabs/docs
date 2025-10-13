5b7e42996aa42180e91dea959b9c92855aa7dde2
Chris Bell <cjbell1988@gmail.com>
2025-10-13 13:38:49 -0400

    fix: always allow unsafe-eval (#1174)

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
 
     const cspHeader = `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' ${isDev ? "'unsafe-eval'" : ""} https:;
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
       connect-src 'self' ${isDev ? "ws:" : ""} https:;
       style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
       img-src 'self' blob: data: https://knock.app;
