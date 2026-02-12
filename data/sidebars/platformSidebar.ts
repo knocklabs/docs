01aa5f4618483f9a168d03dd22648e0393ef94c7
Connor Lindsey <cblindsey3@gmail.com>
2026-02-12 14:31:03 -0800

    feat: add random cohort branch docs

--- data/sidebars/platformSidebar.ts
+++ data/sidebars/platformSidebar.ts
@@ -52,6 +52,11 @@ export const PLATFORM_SIDEBAR: SidebarSection[] = [
       { slug: "/delay-function", title: "Delay function" },
       { slug: "/batch-function", title: "Batch function" },
       { slug: "/branch-function", title: "Branch function" },
+      {
+        slug: "/random-cohort-branch-function",
+        title: "Random cohort branch function",
+        isBeta: true,
+      },
       { slug: "/fetch-function", title: "Fetch function" },
       { slug: "/throttle-function", title: "Throttle function" },
       {
